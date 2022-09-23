f017aed3d899f5cdc1c5542d250c981daf1ab000
dallin00 <20dcmw@queensu.ca>
2022-09-23 14:01:57 -0400

    fixed style

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# check style!!!!!
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+
 
 # edit this to make it properly suit user entity
 class User(db.Model):
@@ -14,11 +13,13 @@ class User(db.Model):
     username = db.Column(db.String(), unique=True, nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
 
+
 # need to edit to make it properly suit guest entity
 class VerifiedGuest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
+
 
 db.create_all()
 
@@ -38,10 +39,10 @@ print("users:")
 for i in users:
     print("id:", i.id)
     print("username:", i.username)
-    print("email:",i.email)
+    print("email:", i.email)
 
 print("guests:")
 for i in guests:
     print("id:", i.id)
     print("username:", i.username)
-    print("email:",i.email)
+    print("email:", i.email)
