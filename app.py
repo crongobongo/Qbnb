c7fd9dadf4b5d5e5909172494e3e62c8d8417a78
bengo3022 <randommanboiman@gmail.com>
2022-09-25 20:36:38 -0400

    Added a few attributes to User classes

--- app.py
+++ app.py
@@ -7,18 +7,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
-# edit this to make it properly suit user entity
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
+    phone_number = db.Column(db.String(), unique=True, nullable=False)
+    payment_method = db.Column(db.String(), nullable=False)
 
 
-# need to edit to make it properly suit guest entity
 class VerifiedGuest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
+    phone_number = db.Column(db.String(), unique=True, nullable=False)
+    payment_method = db.Column(db.String(), nullable=False)
 
 class Review(db.Model):
     reviewer = db.Column(db.Integer, primary_key=True)
@@ -37,11 +39,11 @@ class Listing(db.Model):
 db.create_all()
 
 # add test user to database
-db.session.add(User(username="user1", email="user1@example.com"))
+db.session.add(User(username="user1", email="user1@example.com", phone_number="000-000-0000", payment_method="Credit"))
 db.session.commit()
 
 # add test verified guest to database
-db.session.add(VerifiedGuest(username="guest1", email="guest1@example.com"))
+db.session.add(VerifiedGuest(username="guest1", email="guest1@example.com", phone_number="111-111-1111", payment_method="Cash"))
 db.session.commit()
 
 # add test review to database
@@ -62,13 +64,17 @@ print("users:")
 for i in users:
     print("id:", i.id)
     print("username:", i.username)
-    print("email:",i.email)
+    print("email:", i.email)
+    print("phone#:", i.phone_number)
+    print("payment:", i.payment_method)
 
 print("\nguests:")
 for i in guests:
     print("id:", i.id)
     print("username:", i.username)
-    print("email:",i.email)
+    print("email:", i.email)
+    print("phone#:", i.phone_number)
+    print("payment:", i.payment_method)
 
 print("\nreviews:")
 for i in reviews:
