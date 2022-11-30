ed35e4d740872b8a8b444a0c7fa60438a585a4cb
Christian D'Souza <20ceds@queensu.ca>
2022-11-29 21:58:11 -0500

    frontend testing error messages

--- qbnb_test_frontend/test_create_booking.py
+++ qbnb_test_frontend/test_create_booking.py
@@ -52,7 +52,8 @@ class FrontEndHomePageTest(BaseCase):
         self.click('input[type="submit"]')
         time.sleep(1)  # page should load correctly
         self.assert_element("#create-booking-header")
-        self.assert_text("Cannot book y=our own listing!", "#create-booking-header")
+        self.assert_text("Cannot book y=our own listing!", 
+        "#create-booking-header")
 
     def test_create_booking_input_email2_failure(self, *_):
         
